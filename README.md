d803a8027aac7a540472039da88cb69b24876b0e
Brett Terespolsky <bteres@gmail.com>
2017-03-30 17:16:07 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is an example Meteor 1.3 module.  You should describe it here . . . 
---
+++ index.js
@@ -0,0 +1,4 @@
+// meteor-ci-package/index.js
+exports.meteor-ci-packageLog = function() {
+  console.log("logged from module 'meteor-ci-package'" );
+};
